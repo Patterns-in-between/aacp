fdea58b3af509d2321feaa4a7cad07d8e906e095
alex <alex@slab.org>
2021-05-24 08:54:04 +0100

    python 3 compatibility

--- zmq/forwarder_server.py
+++ zmq/forwarder_server.py
@@ -15,6 +15,6 @@ publisher_id = random.randrange(0,9999)
 while True:
     topic = random.randrange(1,10)
     messagedata = "server#%s" % publisher_id
-    print "%s %s" % (topic, messagedata)
-    socket.send("%d %s" % (topic, messagedata))
+    print("%s %s" % (topic, messagedata))
+    socket.send_string("%d %s" % (topic, messagedata))
     time.sleep(1)
--- zmq/forwarder_subscriber.py
+++ zmq/forwarder_subscriber.py
@@ -9,11 +9,11 @@ port = "5560"
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-print "Collecting updates from server..."
-socket.connect ("tcp://%s:%s" % (server,port))
+print("Collecting updates from server...")
+socket.connect("tcp://%s:%s" % (server,port))
 topicfilter = "9"
-socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
 for update_nbr in range(10):
     string = socket.recv()
     topic, messagedata = string.split()
-    print topic, messagedata
+    print(topic, messagedata)
