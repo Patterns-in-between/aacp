8a3d139c20a1e9d2d9213751c9620446c7802865
alex <alex@slab.org>
2021-05-24 08:49:40 +0100

    set server to slab

---
+++ zmq/forwarder_subscriber.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+import zmq
+
+server = "slab.org"
+port = "5560"
+
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+print "Collecting updates from server..."
+socket.connect ("tcp://%s:%s" % (server,port))
+topicfilter = "9"
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+for update_nbr in range(10):
+    string = socket.recv()
+    topic, messagedata = string.split()
+    print topic, messagedata
