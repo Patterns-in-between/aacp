5fc1458848f3088270e763d08d61a056cf8de862
alex <alex@slab.org>
2023-01-20 16:44:07 +0000

    send spline data

--- py/vis/vis-peak.py
+++ py/vis/vis-peak.py
@@ -27,6 +27,8 @@ samplerate = 25 # TODO - calculate this
 
 osc_target = liblo.Address("localhost", 6010)
 
+spline_target = liblo.Address("localhost", 1819)
+
 half_pi = math.pi/2
 
 segments = 16
@@ -39,7 +41,9 @@ cps = 0.5625
 #cycletime = time.time()
 values = {}
 
-link_clock = linkclock.LinkClock(120, 4, 0)
+last_spline_sent = 0
+
+link_clock = linkclock.LinkClock(120, 8, 0)
 link_clock.start()
 
 def cycle_now():
@@ -117,7 +121,7 @@ while 1:
     if key in history:
         # clear out old history
         for i in range(0,len(history[key])):
-            (t, value) = history[key][i]
+            (t, value, value2) = history[key][i]
             if((now - t) < cycles):
                 history[key] = history[key][i:]
                 break
@@ -126,6 +130,7 @@ while 1:
 
         # Draw line while calculating averages
         average_bins = []
+        average_bins2 = []
         for i in range(0,segments):
             average_bins.append([])
 
@@ -137,7 +142,7 @@ while 1:
                            )[0]
         
         def to_cycle_and_value(i):
-            return ((history[key][i][0]) % 1, history[key][i][1])
+            return ((history[key][i][0]) % 1, history[key][i][1], history[key][i][2])
         
         peak_t = list(map(to_cycle_and_value, peaks))
         peak_t.sort()
@@ -146,7 +151,7 @@ while 1:
         peak_t = list(filter(lambda x: x[1] > 0.1, peak_t))
 
         
-        for (t, value) in history[key]:
+        for (t, value, value2) in history[key]:
             #print("t: %f, value: %f" % (t, value))
             x = (value * 300 * math.cos((t % 1)*math.tau-half_pi))+midx;
             y = (value * 300 * math.sin((t % 1)*math.tau-half_pi))+midy;
@@ -175,14 +180,15 @@ while 1:
         peak_bin_bools = [False] * peak_segments
         
         if len(peak_t) > 0:
+            
             # quantise the peaks
             peak_bins = []
             for i in range(0, peak_segments):
                 peak_bins.append([])
             for x in peak_t:
-                pos, val = x
+                pos, val, val2 = x
                 peak_bin = math.floor(pos * peak_segments)
-                print("peak in bin %d from pos %.2f" % (peak_bin, pos))
+                #print("peak in bin %d from pos %.2f" % (peak_bin, pos))
                 peak_bins[peak_bin].append(val)
                 peak_bin_bools[peak_bin] = True
 
@@ -193,7 +199,7 @@ while 1:
                 else:
                     peak_bin_mini.append("%.4f" % statistics.mean(peak_bins[i]))
 
-            print(" ".join(peak_bin_mini))
+            #print(" ".join(peak_bin_mini))
             liblo.send(osc_target, "/ctrl", "peakbins", " ".join(peak_bin_mini))
         else:
             liblo.send(osc_target, "/ctrl", "peakbins", " ")
@@ -224,13 +230,34 @@ while 1:
             pygame.draw.lines(screen, (255,255,255), False, points, 4)
             
         if len(peak_t) > 0:
+            if math.floor(cycle_now()) > last_spline_sent:
+                last_spline_sent = math.floor(cycle_now())
+                # for sending peaks to be splined
+                to_spline = []
+                for x in peak_t:
+                    (p, val, val2) = x
+
+                    peakx = ((val * math.cos((p%1)*math.tau-half_pi)) + 1) / 2;
+                    peaky = ((val * math.sin((p%1)*math.tau-half_pi)) + 1) / 2;
+                    # val2 just used for sorting
+                    to_spline.append((val2, peakx, peaky))
+
+                spline_vals = []
+                for (val2, x, y) in to_spline:
+                    spline_vals.append(x)
+                    spline_vals.append(y)
+                print(cycle_now())
+                print("send splines")
+                print(spline_vals)
+                liblo.send(spline_target, "/splinepeaks", *spline_vals)
+            
             # turn peak times into a list of durations with values,
             # drawing the peaks as circles as we go..
             peak_durs = []
             peak_durs.append((peak_t[0][0], None))
             
             for i, x in enumerate(peak_t):
-                (p, val) = x
+                (p, val, val2) = x
 
                 x = (val * 300 * math.cos((p%1)*math.tau-half_pi))+midx;
                 y = (val * 300 * math.sin((p%1)*math.tau-half_pi))+midy;
@@ -251,8 +278,10 @@ while 1:
                 else:
                     peak_mini.append("%.4f@%.4f" % (val, max(dur, 0.0001)))
             joined_mini = " ".join(peak_mini)
-            print(joined_mini)
+            #print(joined_mini)
             liblo.send(osc_target, "/ctrl", "peaks", joined_mini)
+        else:
+            liblo.send(osc_target, "/ctrl", "peaks", " ")
             
     # pygame.draw.rect(screen, white, (100,100,200,200))
     #pygame.draw.polygon(screen, white, ((100,100),(200,200),(300,100)))
@@ -280,7 +309,7 @@ while 1:
             #print("ah: " + str(a))
             if not name in history:
                 history[name] = []
-            history[name].append((now, b))
+            history[name].append((now, a, b))
         if msg == 'rand':
             if not 'rand' in history:
                 history['rand'] = []
